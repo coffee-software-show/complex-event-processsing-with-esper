7d755c6a80c792bec646f81e7a71d6090152461e
Josh Long <josh@joshlong.com>
2023-01-17 18:12:13 -0500

    Update CustomerCreatedEvent.java

--- src/main/java/bootiful/CustomerCreatedEvent.java
+++ src/main/java/bootiful/CustomerCreatedEvent.java
@@ -1,18 +1,4 @@
 package bootiful;
 
-/*
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class CustomerCreatedEvent {
-
-	private String name;
-
-	private int age;
-
-}
-*/
-
 public record CustomerCreatedEvent(String name, int age) {
 }
